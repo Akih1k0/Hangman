fa4f76fd24dd0c7f832e8a25acba4086f4e07c38
Akihiko <bsami02@outlook.com>
2023-10-21 19:36:38 +0100

    Optimises the code using abstraction

--- milestone_2.py
+++ milestone_2.py
@@ -1,14 +1,18 @@
 import random
-# Created fruit list
-word_list = ['Apple', 'Banana', 'Pear', 'Kiwi', 'Grape']
-print(word_list)
-# Choose random word from list
-word = random.choice(word_list)
-print(word)
-# Input to guess a letter
-guess = input('Enter a single letter: ')
-# Checks if the letter is a single letter and in the alphabet
-if len(guess) == 1 and guess.isalpha():
-    print('Good guess!')
-else:
-    print('Oops! That is not a valid input')
+
+def random_word():
+
+    word_list = ['Apple', 'Banana', 'Pear', 'Kiwi', 'Grape']
+    word = random.choice(word_list)
+    print(word)
+
+def guess_input():
+
+    guess = input('Enter a single letter: ')
+    if len(guess) == 1 and guess.isalpha():
+        print('Good guess!')
+    else:
+        print('Oops! That is not a valid input')
+
+random_word()
+guess_input()
